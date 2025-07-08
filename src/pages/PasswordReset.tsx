0605cb266ddac192e346432a192ca28e9f6a0962
Mehemmed77 <mehemmedkazimzade54@gmail.com>
2025-07-08 18:11:48 +0400

    started password reset

---
+++ src/pages/PasswordReset.tsx
@@ -0,0 +1,48 @@
+import { Box, Paper, Stack, Typography } from "@mui/material";
+import MotionDivWrapper from "../components/MotionDivWrapper";
+import RsTypography from "../components/ui/RsTypography";
+import RsInput from "../components/ui/RsInput";
+import { useForm } from "react-hook-form";
+import RsButton from "../components/ui/RsButton";
+
+interface FormData {
+    email: string;
+}
+
+export default function PasswordReset() {
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+
+    const onSave = (data: FormData) => {
+        console.log(data.email);
+    }
+
+    return (
+        <MotionDivWrapper>
+            <Box className="passwordResetBox">
+                <form onSubmit={handleSubmit(onSave)}>
+                    <Paper sx={{ width: "100%", maxWidth: "600px", padding:"20px", borderRadius: "20px" }}>
+                    <Stack spacing={3}>
+                        <RsTypography lg="30px" xs="24px" fontWeight="bold" 
+                        text="Enter your email address to receive a verification code" 
+                        gutterBottom textAlign={"center"} />
+
+                        <RsInput 
+                            label="Your Email"
+                            type="email"
+                            placeholder="....@gmail.com"
+                            register={register("email", {
+                                required: "Please fill the email",
+                                validate: value => value.includes("@") || "Email must contain '@' symbol." 
+                            })}
+                        />
+        
+                        <Typography color="error"> { errors.email?.message } </Typography>
+
+                        <RsButton text={"Submit email"} type="submit" />
+                    </Stack>
+                </Paper>
+                </form>
+            </Box>
+        </MotionDivWrapper>
+    )
+}
--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -3,10 +3,12 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Logout from "../pages/Logout";
 import LogoutSuccess from "../pages/LogoutSuccess";
+import PasswordReset from "../pages/PasswordReset";
 
 export default [
     <Route path="/login/" element={<Login />} />,
     <Route path="/register/" element={<Register />} />,
     <Route path="/logout/" element={<Logout />} />,
     <Route path="/logoutSuccess/" element={<LogoutSuccess />} />,
+    <Route path="/passwordReset/" element={<PasswordReset />} />,
 ];
