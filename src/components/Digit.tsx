9864ff7a7e5caf7892d557528ac09181588df37c
Mehemmed77 <mehemmedkazimzade54@gmail.com>
2025-07-10 12:30:32 +0400

    otp is done

--- src/components/Digit.tsx
+++ src/components/Digit.tsx
@@ -1,21 +1,38 @@
 import { TextField } from "@mui/material";
-import type React from "react";
+import React from "react";
 import { useState } from "react";
 
-export default function Digit() {
+interface DigitProps {
+    place: number,
+    handleFocus: (n: number) => void,
+}
+
+const Digit = React.forwardRef<HTMLInputElement, DigitProps>(({ place, handleFocus }, ref) => {
     const [value, setValue] = useState<string>("");
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if(e.key === "Backspace" && value === "" && place !== 0){
+            handleFocus(place - 1);
+        }
+    }
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const inputValue = e.target.value;
         const regEx = /^[0-9]*$/;
-        if (value.length == 1 || !regEx.test(inputValue)) return;
+        if (inputValue.length > 1 || !regEx.test(inputValue)) return;
 
         setValue(inputValue);
+
+        if(inputValue.length === 1 && place !== 5) {
+            handleFocus(place + 1);
+        }
     }
 
     return (
         <TextField
+            inputRef={ref}
             value={value}
+            onKeyDown={handleKeyDown}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e)}
             slotProps={{
                 input: {
@@ -25,4 +42,6 @@ export default function Digit() {
           variant="outlined"
         />
     )
-}
+})
+
+export default Digit;
--- src/pages/SixDigits.tsx
+++ src/pages/SixDigits.tsx
@@ -1,15 +1,20 @@
 import { Box } from "@mui/material";
 import Digit from "../components/Digit";
+import React from "react";
 
 export default function SixDigits() {
+    const refs: React.RefObject<HTMLInputElement | null>[] = Array(6).fill(null).
+            map(() => React.createRef<HTMLInputElement>());
+
+    const handleFocus = (n: number) => {
+        refs[n].current?.focus();
+    }
+
     return (
         <Box>
-            <Digit />
-            <Digit />
-            <Digit />
-            <Digit />
-            <Digit />
-            <Digit />
+            {refs.map((ref, idx) => (
+                <Digit key={idx} ref={ref} handleFocus={handleFocus} place={idx}  />
+            ))}
         </Box>
     )
 }
